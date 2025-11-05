a3f58b7b69b97a4f6ea01cf3c0ec0ace6173654c
John Nkp <john.nkp1@gmail.com>
2025-11-05 12:42:47 +1100

    updated backend default port

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Backend server running on port ${PORT}`);
 });
